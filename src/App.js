82c5bb463414792cfcf5c9bd2f8dda8e7d394fc1
vanjaandrejic <vanjaandrejic5@gmail.com>
2022-09-27 15:19:14 +0200

    Fetch data form jsonplaceholder and Header component impelemntation

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import React from "react";
+import { useEffect, useState } from "react";
+
+import Header from "./Components/Header";
+
 function App() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      let res = await fetch("https://jsonplaceholder.typicode.com/posts");
+      let data = await res.json();
+      setPosts(data);
+    };
+    fetchPosts();
+  }, []);
+
   return (
-    <div>
-    </div>
+    <>
+      <Header posts={posts}/>
+    </>
   );
 }
 
---
+++ src/Components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../Styles/Header.css";
+
+export default function Header(props) {
+  const postsNumberCounter = props.posts.length;
+
+  return (
+    <div className="header">
+      <div className="header-title">
+        <h1>Posts found: {postsNumberCounter}</h1>
+      </div>
+    </div>
+  );
+}
